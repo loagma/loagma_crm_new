f7547d8376046554b1f80bddb2db4c5d068467ef
anupamkl14 <anupamkumarl1464@gmail.com>
2026-06-10 15:14:31 +0530

    fix: Ensure the user model is correctly associated with the 'user' table

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -13,6 +13,8 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+    protected $table = 'user';
+
     /**
      * The attributes that are mass assignable.
      *
